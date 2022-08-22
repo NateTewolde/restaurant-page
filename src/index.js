b69942a9393029751d03079e7e144a0eba414cfb
NateTewolde <tewolde.nate@gmail.com>
2022-08-21 21:55:03 -0700

    Updated to view contact page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import createHomepage from "./home-page";
 import createMenu from "./menu-page";
+import createContactPage from "./contact-page";
 
 //createHomepage();
-createMenu();
+createContactPage();
