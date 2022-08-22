31abf5d63b06de7a425cf399528c32e2937eadd6
NateTewolde <tewolde.nate@gmail.com>
2022-08-21 20:12:52 -0700

    Create a module that creates the resturant homepage and exports it

---
+++ src/home-page.js
@@ -0,0 +1,30 @@
+function createHomepage() {
+  const content = document.querySelector("#content");
+
+  const homePage = document.createElement("div");
+  homePage.classList.add("home-page");
+
+  const restaurantImg = document.createElement("img");
+  restaurantImg.src = "../src/images/restaurant-image.jpg";
+
+  const restaurantHeadline = document.createElement("div");
+  restaurantHeadline.classList.add("headline");
+
+  const restaurantName = document.createElement("div");
+  restaurantName.classList.add("restaurant-name");
+  restaurantName.textContent = "Corner Bistro";
+
+  const restaurantSlogan = document.createElement("div");
+  restaurantSlogan.classList.add("restaurant-Slogan");
+  restaurantSlogan.textContent =
+    "When you're here, you're family. Eat Fresh. Have it your way.";
+
+  restaurantHeadline.appendChild(restaurantName);
+  restaurantHeadline.appendChild(restaurantSlogan);
+
+  homePage.appendChild(restaurantImg);
+  homePage.appendChild(restaurantHeadline);
+  content.appendChild(homePage);
+}
+
+export default createHomepage;
