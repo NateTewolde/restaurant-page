c8fa3f77939fce5ccec533a8d2fe762a57ff3be2
NateTewolde <tewolde.nate@gmail.com>
2022-08-22 01:21:06 -0700

    Add contact info section with an email and phone number div

--- src/contact-page.js
+++ src/contact-page.js
@@ -7,7 +7,19 @@ function createContactPage() {
   const contactImg = document.createElement("img");
   contactImg.src = "../src/images/restaurant-staff.jpg";
 
+  const contactInfo = document.createElement("div");
+  contactInfo.classList.add("contact-info-section");
+
+  const email = document.createElement("div");
+  email.textContent = "bistro@bistro-restaurant.com";
+  contactInfo.appendChild(email);
+
+  const phone = document.createElement("div");
+  phone.textContent = "555-555-5555";
+  contactInfo.appendChild(phone);
+
   contactPage.appendChild(contactImg);
+  contactPage.appendChild(contactInfo);
   content.appendChild(contactPage);
 }
 
