308f92f43940b8b976227ffb0cecb68d0b9f5049
NateTewolde <tewolde.nate@gmail.com>
2022-08-21 19:26:06 -0700

    Create a homepage function that displays a resturant image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log("hi there");
+
+const content = document.querySelector("#content");
+createHomepage();
+function createHomepage() {
+  const homePage = document.createElement("div");
+  homePage.classList.add("home-page");
+
+  const restaurantImg = document.createElement("img");
+  restaurantImg.src = "../src/images/restaurant-image.jpg";
+
+  homePage.appendChild(restaurantImg);
+  content.appendChild(homePage);
+}
