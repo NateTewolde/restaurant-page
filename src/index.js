78cdf2ed204794aef2f8d3897d49b0b8cffb5d02
NateTewolde <tewolde.nate@gmail.com>
2022-08-22 02:02:16 -0700

    Create formatTabs function that correctly adds an event listener to each tab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import createHomepage from "./home-page";
 import createMenu from "./menu-page";
 import createContactPage from "./contact-page";
-
+console.log("hik");
 function createTabs() {
   const content = document.querySelector("#content");
 
@@ -9,17 +9,37 @@ function createTabs() {
   tabs.classList.add("tabs");
 
   const homeTab = document.createElement("div");
+  homeTab.classList.add("home-tab");
+  homeTab.classList.add("tab");
   homeTab.textContent = "Home";
   tabs.appendChild(homeTab);
+  homeTab.setAttribute("data-tab-id", "1");
 
   const menuTab = document.createElement("div");
+  menuTab.classList.add("menu-tab");
+  menuTab.classList.add("tab");
   menuTab.textContent = "Menu";
   tabs.appendChild(menuTab);
+  menuTab.setAttribute("data-tab-id", "2");
 
   const contactTab = document.createElement("div");
+  contactTab.classList.add("contact-tab");
+  contactTab.classList.add("tab");
   contactTab.textContent = "Contact";
   tabs.appendChild(contactTab);
   content.appendChild(tabs);
+  contactTab.setAttribute("data-tab-id", "3");
 }
 createTabs();
-createHomepage();
+
+function formatTabs() {
+  const tabs = document.querySelectorAll("[data-tab-id]");
+  tabs.forEach((tab) => {
+    console.log("test");
+    tab.addEventListener("click", () => {
+      console.log(tab);
+    });
+  });
+}
+
+formatTabs();
