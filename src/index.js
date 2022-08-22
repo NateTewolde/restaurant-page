fb6a4ad8e8607788f66349ee3ec2435f292b22da
NateTewolde <tewolde.nate@gmail.com>
2022-08-22 01:22:52 -0700

    Add createTabs function for displaying module tabs

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import createHomepage from "./home-page";
 import createMenu from "./menu-page";
 import createContactPage from "./contact-page";
 
-//createHomepage();
-createContactPage();
+function createTabs() {
+  const content = document.querySelector("#content");
+
+  const tabs = document.createElement("div");
+  tabs.classList.add("tabs");
+
+  const homeTab = document.createElement("div");
+  homeTab.textContent = "Home";
+  tabs.appendChild(homeTab);
+
+  const menuTab = document.createElement("div");
+  menuTab.textContent = "Menu";
+  tabs.appendChild(menuTab);
+
+  const contactTab = document.createElement("div");
+  contactTab.textContent = "Contact";
+  tabs.appendChild(contactTab);
+  content.appendChild(tabs);
+}
+createTabs();
+createHomepage();
